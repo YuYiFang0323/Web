34fd538c38eb747dd717fc095e3af2a1e9e621a4
AnnaLee1215 <ec460520@gmail.com>
2023-01-04 15:01:19 +0800

    0

---
+++ NTUNHS/src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -0,0 +1,15 @@
+package hitstd.group.tool.database;
+
+public class DBConfig {
+	public DBConfig(){		
+	}
+	
+	public String FilePath(){
+	   //DB FilePath
+		 //String DBPath="C:\\Users\\User\\Documents\\GitHub\\eating\\test\\src\\main\\webapp\\member.accdb";
+		 
+	   //DB FilePath
+		 String DBPath="C:\\Users\\User\\Documents\\GitHub\\eating\\test\\src\\main\\webapp\\member.accdb";
+	   return DBPath;		
+	}
+}
