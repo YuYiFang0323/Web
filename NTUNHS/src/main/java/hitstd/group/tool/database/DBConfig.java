8f9c71f5c6e41e79ce23be3793724e2ae221bf6e
YuYiFang0323 <wanglong51314225@gmail.com>
2023-01-10 23:43:34 +0800

    0

--- NTUNHS/src/main/java/hitstd/group/tool/database/DBConfig.java
+++ NTUNHS/src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -5,17 +5,12 @@ public class DBConfig {
 	}
 	
 	public String FilePath(){
-<<<<<<< HEAD
+
 	   //DB FilePath
 
 		 String DBPath="C:\\Users\\m\\Documents\\GitHub\\Web\\NTUNHS\\src\\main\\webapp\\Ntunhs.accdb";   //游
 		//String DBPath="C:\\Users\\User\\Documents\\GitHub\\Web\\NTUNHS\\src\\main\\webapp\\Ntunhs.accdb";  //黃
-=======
-	   //DB FilePath	
-		// String DBPath="C:\\Users\\m\\Documents\\GitHub\\Web\\NTUNHS\\src\\main\\webapp\\Ntunhs.accdb";   //游
-		 String DBPath="C:\\Users\\User\\Documents\\GitHub\\Web\\NTUNHS\\src\\main\\webapp\\Ntunhs.accdb";  //黃
->>>>>>> bdd654a020f218a29d28ca2f1471e695ca445aa3
-		// String DBPath="C:\\apache-tomcat-9.0.54\\webapps\\NTUNHS\\Ntunhs.accdb";
+	    // String DBPath="C:\\apache-tomcat-9.0.54\\webapps\\NTUNHS\\Ntunhs.accdb";
 	   return DBPath;		
 	}
 
